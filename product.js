f7a7d3ab429d641c818f61081f2355c41ac628ad
Nguyentan0107 <07tom91@gmail.com>
2022-12-28 06:40:15 +0700

    quanlysanpham

---
+++ product.js
@@ -0,0 +1,40 @@
+class Product {
+    name;
+    price;
+    number;
+    img;
+
+
+    constructor(name, price, number, img) {
+        this.name = name;
+        this.price = price;
+        this.number = number;
+        this.img = img;
+    }
+
+
+    get name() {
+        return this.name;
+    }
+
+    set name(value) {
+        this.name = value;
+    }
+
+    get price() {
+        return this.price;
+    }
+
+    set price(value) {
+        this.price = value;
+    }
+
+    get number() {
+        return this.number;
+    }
+
+    set number(value) {
+        this.number = value;
+    }
+}
+
---
+++ productManager.js
@@ -0,0 +1,49 @@
+// let products = [
+//     new Product("Iphone 13", 28000, 30,"anh 1"),
+//     new Product("Macbook pro M2", 35000, 20,),
+//     new Product("Gaming....", 30000, 10)
+// ];
+// C: Create
+// R: Read (list, detail)
+// U: Update
+// D: Delete
+
+
+
+//Ham hien thi danh sach
+function showAllProducts() {
+    let content = "";
+    for (let i = 0; i < products.length; i++) {
+        content+= ` <tr>
+                    <td>${products[i].name}</td>
+                    <td>${ products[i].price}</td>
+                    <td>${ products[i].number}</td>
+                    <td><img src='"+products[i].img+"'></td>
+                    <td><button>Edit</button></td>
+                    <td><button onclick='deleteByIndex("+i+")'>Delete</button></td>
+                </tr>`
+    }
+    //ghi lai bang innerHTML
+    document.getElementById("listProduct").innerHTML = content;
+    document.getElementById("numberOfProduct").innerText = products.length;
+}
+showAllProducts();
+function createNewProduct() {
+    //b1: lay du lieu trong the input
+    let name = document.getElementById("productName").value;
+    let price = document.getElementById("price").value;
+    let number = document.getElementById("number").value;
+    let img = document.getElementById("img").value;
+    let newP = new Product(name, price, number, img);
+    //b2: them moi vao mang
+    products.push(newP);
+    //b3: Ve lai bang
+    showAllProducts();
+}
+//xoa theo index
+function deleteByIndex(index) {
+    // xoa phan tu
+    products.splice(index, 1);
+    // in lai mang
+    showAllProducts();
+}
